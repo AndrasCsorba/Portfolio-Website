10d3e995fc465e98fe2d919a7955dbcb8fe3b0b0
AndrasCsorba <andreascs@mail.ch>
2025-03-08 02:40:49 +0100

    typewriting logic

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,9 +1,26 @@
+import { use } from "react";
+
 export const LoadingScreen = () => {
+  const [text, setText] = useState;
+  const fullText = "<Helolo world />";
+
+  useEffect(() => {
+    let index = 0;
+    const interval = setInterval(() => {
+      setText(fullText.substring(0, index));
+      index++;
+
+      if (index > fullText.length) {
+        clearInterval(interval);
+      }
+    }, 100);
+    return () => clearInterval(interval);
+  });
+
   return (
     <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
       <div className="mb-4 text-4xl font-mono font-bold">
-        {" "}
-        Helolo world
+        {text}
         <span className="animate-blink ml-1"></span>
       </div>
       <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
